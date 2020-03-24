480aa0852b5796d8b1a12840c6379dd1bedd20e4
James Alexander <j.alex@email.com>
2020-03-24 10:52:27 -0700

    Removed the division operation.

--- lib/src/helpers/round_values.dart
+++ lib/src/helpers/round_values.dart
@@ -1,3 +1,4 @@
-/// Rounds the [value] to the [precision]th.
-num round(num value, [int precision = 1000000]) =>
-    (value * precision).round() / precision;
+/// Rounds `value * precision`.
+///
+/// Used by the equality operators for every color model except RGB.
+num round(num value, [int precision = 1000000]) => (value * precision).round();
