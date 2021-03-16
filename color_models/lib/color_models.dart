4a12c7563ea56f4bc980b83ce435d272fc9f0fd0
James Alexander <j.alex@email.com>
2021-03-16 00:40:24 -0700

    Add documentation to the libraries.

--- color_models/lib/color_models.dart
+++ color_models/lib/color_models.dart
@@ -1,3 +1,6 @@
+/// A set of classes representing the CMYK, HSI, HSL, HSP, HSB, LAB,
+/// Oklab, RGB, and XYZ color spaces with methods to convert colors
+/// between each space.
 library color_models;
 
 export 'src/color_model.dart';
--- flutter_color_models/lib/flutter_color_models.dart
+++ flutter_color_models/lib/flutter_color_models.dart
@@ -1,3 +1,6 @@
+/// A set of classes representing the CMYK, HSI, HSL, HSP, HSB, LAB,
+/// Oklab, RGB, and XYZ color spaces with methods to convert colors
+/// between each space.
 library flutter_color_models;
 
 export 'package:color_models/color_models.dart' show ColorSpace;
